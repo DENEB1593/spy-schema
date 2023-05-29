ff4ae53e79b69477f728eed7e72c7a664761d23c
deneb1593 <deneb1593@gmail.com>
2023-05-29 15:25:58 +0900

    불필요 import 제거

--- src/main/java/io/dev/deneb/web/user/JoinRequest.java
+++ src/main/java/io/dev/deneb/web/user/JoinRequest.java
@@ -1,11 +1,4 @@
 package io.dev.deneb.web.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonSerialize
 public record JoinRequest(String name) {
 }
--- src/main/java/io/dev/deneb/web/user/JoinResponse.java
+++ src/main/java/io/dev/deneb/web/user/JoinResponse.java
@@ -1,7 +1,4 @@
 package io.dev.deneb.web.user;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record JoinResponse(Long id, String name) {
 }
