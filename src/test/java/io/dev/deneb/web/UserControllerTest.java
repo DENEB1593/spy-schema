3388ecce3e6def96f95b47873a815be96e47ce4c
deneb1593 <deneb1593@gmail.com>
2023-06-03 15:14:33 +0900

    post array 파싱 테스트

--- src/test/java/io/dev/deneb/web/UserControllerTest.java
+++ src/test/java/io/dev/deneb/web/UserControllerTest.java
@@ -2,6 +2,7 @@ package io.dev.deneb.web;
 
 import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dev.deneb.entity.Post;
 import io.dev.deneb.web.user.JoinRequest;
 import io.dev.deneb.web.user.JoinResponse;
 import lombok.SneakyThrows;
@@ -11,6 +12,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -31,13 +35,13 @@ class UserControllerTest {
 
         var request = new JoinRequest("test");
 
-        var ra = mvc.perform(post("/api/users/join")
+        var result = mvc.perform(post("/api/users/join")
                         .contentType(APPLICATION_JSON)
                         .content(JSON.toJSONString(request)))
                 .andExpect(status().isCreated());
 
         var response = JSON.parseObject(
-                ra.andReturn().getResponse().getContentAsString(),
+                result.andReturn().getResponse().getContentAsString(),
                 JoinResponse.class
         );
 
@@ -54,9 +58,16 @@ class UserControllerTest {
     @Test
     @SneakyThrows
     void shouldGetPosts() {
-        mvc.perform(
-                get("/api/users/" + 1L))
+        var result = mvc.perform(get("/api/users/" + 1L))
                 .andExpect(status().isOk());
+
+        var posts = JSON.parseArray(
+                result.andReturn().getResponse().getContentAsString(),
+                Post.class
+        );
+
+
+        assertThat(posts).hasSize(3);
     }
 
 
