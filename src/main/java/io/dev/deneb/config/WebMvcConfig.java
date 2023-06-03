6059d32899ce857a03da0ae3fed5384fd43b4db3
deneb1593 <deneb1593@gmail.com>
2023-06-03 15:05:11 +0900

    fastjson 적용

---
+++ src/main/java/io/dev/deneb/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package io.dev.deneb.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        converters.add(0, fastJsonHttpMessageConverter);
+    }
+}
--- src/test/java/io/dev/deneb/web/UserControllerTest.java
+++ src/test/java/io/dev/deneb/web/UserControllerTest.java
@@ -1,14 +1,18 @@
 package io.dev.deneb.web;
 
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dev.deneb.web.user.JoinRequest;
+import io.dev.deneb.web.user.JoinResponse;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -21,28 +25,34 @@ class UserControllerTest {
     @Autowired
     MockMvc mvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
-
     @Test
     @SneakyThrows
     void shouldJoinUserAndReturnCreated() {
 
         var request = new JoinRequest("test");
 
-        mvc.perform(post("/api/users/join")
+        ResultActions ra = mvc.perform(post("/api/users/join")
                         .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.name").exists());
+                        .content(JSON.toJSONString(request)))
+                .andExpect(status().isCreated());
+
+        JoinResponse response = JSON.parseObject(
+                ra.andReturn().getResponse().getContentAsString(),
+                JoinResponse.class
+        );
+
+        assertThat(response).isNotNull();
+        assertThat(response.name()).isNotNull();
+        assertThat(response.id()).isNotNull();
+
+//                .andExpect(jsonPath("$.id").exists())
+//                .andExpect(jsonPath("$.name").exists());
     }
 
 
     @Test
     @SneakyThrows
     void shouldGetPosts() {
-
         mvc.perform(
                 get("/api/users/" + 1L))
                 .andExpect(status().isOk());
